8afc5f727148f48d02e62a5669be2f055b6899b7
Arung Ikhsani Wijaya <100556943+ArungIW2@users.noreply.github.com>
2026-09-15 03:19:46 +0700

    feat: complete order domain relationships

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -29,4 +29,5 @@ class Order extends Model
 
     public function user(): BelongsTo { return $this->belongsTo(User::class); }
     public function items(): HasMany { return $this->hasMany(OrderItem::class); }
+    public function statusHistories(): HasMany { return $this->hasMany(OrderStatusHistory::class); }
 }
